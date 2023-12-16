f83db124591161b99f8e378146349d2ee4b355df
Agniva Bhattacharya <agnivabhattacharya2001@gmail.com>
2023-12-16 16:49:17 +0530

    Debbuging Purpose

--- src/Banner.js
+++ src/Banner.js
@@ -1,7 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Banner.css'
 import image1 from './image1.png';
+import { useEffect } from 'react';
+// import { set } from 'immer/dist/internal';
+
+
 function Banner() {
+  
+  const[movieArray,setMovieArray]=useState([]);
+  const[movieDetails,setMovieDetails]=useState([]);
+
+
+  const handleStateChange=async (response)=>{
+    // await setMovieArray(s =>
+    //    {
+    //     console.log(response.results);
+    //     return response.results;
+       
+    //   });
+    // const something=async ()=>{
+    //   await setMovieArray(response.results);  
+    // }
+    await setMovieArray(response.results);
+    // await something();
+
+    // console.log(movieArray);
+  }
+
+
+  
+  function fetchData(){
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYmFjZmVkMTZkYjU4OTQ0ZjcxNTU2ZDg4Nzg3NmM5ZiIsInN1YiI6IjY1N2MzYTIxZWM4YTQzMDEzNzAxMWE4YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.quPsXIOlk4hXg-MXVPn5_FW9G3NW9c6j-54LWVyylVA'
+      }
+    };
+    
+    fetch('https://api.themoviedb.org/3/trending/all/day?language=en-US', options)
+      .then(response => response.json())
+      .then(response => {
+        // console.log(response);
+        handleStateChange(response);
+        // console.log(movieArray.results);
+        // console.log(movieArray.results.length)
+        // const randidx=Math.floor(Math.random() * ( movieArray.results.length));
+        // setMovieDetails(movieArray.results[randidx]);
+        // console.log(movieArray.results)
+        // console.log(movieArray.results.length,randidx,movieArray.results)
+      })
+      .catch(err => console.error(err));
+    
+  }
+  
+
+
+
+  useEffect(()=>{
+    
+
+    // async function handleStateChange(response) {
+    //   await setMovieArray(response.results);
+    //   await console.log(movieArray);
+    // }
+
+    
+    fetchData();
+    // console.log(movieArray);
+
+  },[])
+
+  useEffect(()=>{
+    console.log(movieArray);
+    console.log("lund");
+  },[movieArray])
+  // const movebg=movieDetails.
   return (
   <header className="banner" style={{
     backgroundSize: "cover",
